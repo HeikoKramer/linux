6c7d578aeb889c804d64e70c4e118c5730da7a2e
HeikoKramer <github@myforce.net>
2021-02-21 23:04:29 +0100

    vim set as manpager

--- dotfiles/config.bash
+++ dotfiles/config.bash
@@ -180,3 +180,6 @@ if [ -f ~/.sfdx.conf ]; then
     . ~/.sfdx.conf
     echo "sourced >> .sfdx.conf"
 fi
+
+### "vim" as manpager
+export MANPAGER='/bin/bash -c "vim -MRn -c \"set buftype=nofile showtabline=0 ft=man ts=8 nomod nolist norelativenumber nonu noma\" -c \"normal L\" -c \"nmap q :qa<CR>\"</dev/tty <(col -b)"'
