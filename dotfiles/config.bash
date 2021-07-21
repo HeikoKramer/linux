d3f638a885e9314a3f39b8056710653716500342
HeikoKramer <github@myforce.net>
2021-07-21 03:43:01 +0200

    conky excluded

--- dotfiles/config.bash
+++ dotfiles/config.bash
@@ -242,5 +242,3 @@ fi
 
 ### "vim" as manpager
 export MANPAGER='/bin/bash -c "vim -MRn -c \"set buftype=nofile showtabline=0 ft=man ts=8 nomod nolist norelativenumber nonu noma\" -c \"normal L\" -c \"nmap q :qa<CR>\"</dev/tty <(col -b)"'
-
-conky
